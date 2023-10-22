2b13fa4d261dbca6e7274c0cc085efe9f0c585d9
Colette <hello@colettesmith.com>
2023-10-22 14:46:02 +0100

    second vending page

--- src/pages/Vending2.jsx
+++ src/pages/Vending2.jsx
@@ -4,9 +4,64 @@ import "../styles/styles.css";
 const Vending2 = () => {
   return (
     <>
-      <div>
-        <p>screen 4</p>
-      </div>
+      <section className="contents ">
+        <div className="success1Cont">
+          <div classname="cartTitle">
+            <h3>Success</h3>
+          </div>
+
+          <div className="thumbWrapper">
+            <div className="thumbCont">
+              <img src="src/assets/itemBlue.png" alt="blue item" />
+
+              <div className="numberItems">
+                <p>1</p>
+              </div>
+            </div>
+            <div className="thumbCont">
+              <img src="src/assets/itemBlue.png" alt="blue item" />
+              <div className="numberItems">
+                <p>1</p>
+              </div>
+            </div>
+            <div className="thumbCont">
+              <img src="src/assets/itemBlue.png" alt="blue item" />
+              <div className="numberItems">
+                <p>1</p>
+              </div>
+            </div>
+          </div>
+          <div className="feedback">
+            <p>We would love your feedback</p>
+          </div>
+
+          <div className="stars">
+            <img src="src/assets/ph_star-bold.svg" alt="stars Solid"></img>
+            <img src="src/assets/ph_star-bold.svg" alt="stars Solid"></img>
+            <img src="src/assets/ph_star-bold.svg" alt="stars Solid"></img>
+            <img src="src/assets/ph_star-bold.svg" alt="stars Solid"></img>
+            <img src="src/assets/ph_star-bold.svg" alt="stars Solid"></img>
+          </div>
+
+          <div>
+            <h3>Please collect your purchase</h3>
+          </div>
+          <div className="fridge">
+            <div className="fridgeWindow"></div>
+            <div className="fridgeIcons">
+              <div className="chevrons">
+                <img src="src/assets/chevrons.svg" alt="Chevron Icon" />
+              </div>
+              <div className="dispenser">
+                <img src="src/assets/dispenser.png" alt="Chevron Icon" />
+              </div>
+            </div>
+            <Link to="/Checkout">
+              <button className="collect">COLLECT HERE</button>
+            </Link>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
