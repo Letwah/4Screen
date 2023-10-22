759d2a70728ef10b32afc63a4f3c0bd381840376
Colette <hello@colettesmith.com>
2023-10-22 16:11:54 +0100

    tweaks for outline button hover state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+
 import { useSelector, useDispatch } from "react-redux";
 import { selectPage, setPage } from "./features/appSlice";
 
